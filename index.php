1d2cadd78eadac0326fd781313c4ed7e92304046
Pavlov Aleksey <apavlov@lux.ltcompany.com>
2019-12-27 10:44:52 +0300

    path

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ define("USERNAME", "o.mihaylova@ltcompany.com.erp");
 define("PASSWORD", "Boss0804");
 define("SECURITY_TOKEN", "sdfhkjwrhgfwrgergp");
 
-require_once ('soapclient/SforceEnterpriseClient.php');
+require_once ('./Force.com-Toolkit-for-PHP/soapclient/SforceEnterpriseClient.php');
 
 $mySforceConnection = new SforceEnterpriseClient();
 $mySforceConnection->createConnection("wsdl.xml");
