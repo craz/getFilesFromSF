8d969134ffb49bab9d97709c3e6b4f1841f2802d
Pavlov Aleksey <apavlov@lux.ltcompany.com>
2019-12-27 10:39:21 +0300

    login to sf

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-require_once ('Force.com-Toolkit-for-PHP/soapclient/SforceEnterpriseClient.php');
+define("USERNAME", "o.mihaylova@ltcompany.com.erp");
+define("PASSWORD", "Boss0804");
+define("SECURITY_TOKEN", "sdfhkjwrhgfwrgergp");
+
+require_once ('soapclient/SforceEnterpriseClient.php');
+
+$mySforceConnection = new SforceEnterpriseClient();
+$mySforceConnection->createConnection("wsdl.xml");
+$mySforceConnection->login(USERNAME, PASSWORD.SECURITY_TOKEN);
 
