69a085a63c05ffc971f4b00f53c9c12eec6b9827
Pavlov Aleksey <apavlov@lux.ltcompany.com>
2019-12-27 13:12:12 +0300

    not message

--- index.php
+++ index.php
@@ -3,9 +3,12 @@ define("USERNAME", "o.mihaylova@ltcompany.com.erp");
 define("PASSWORD", "Boss0804");
 define("SECURITY_TOKEN", "sdfhkjwrhgfwrgergp");
 
-require_once ('./soapclient/SforceEnterpriseClient.php');
-
+require_once ('/soapclient/SforceEnterpriseClient.php');
+try{
 $mySforceConnection = new SforceEnterpriseClient();
 $mySforceConnection->createConnection("wsdl.xml");
 $mySforceConnection->login(USERNAME, PASSWORD);
+}catch(Exception $e){
+    echo $e->getMessage();
+}
 
